d2b6388f26f88f309f37c977fc655a6aafe39555
Pokemaobr <contato@pokemaobr.dev>
2024-09-20 04:14:15 -0300

    atualizando

--- public/google.php
+++ public/google.php
@@ -7,28 +7,37 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method == 'POST') {
 
     $requestBody = file_get_contents('php://input');
-    //$json = json_decode($requestBody);
-    //$intent = $json->queryResult->intent->displayName;
+    $json = json_decode($requestBody);
+    $intent = $json->queryResult->intent->displayName;
 
 
-/*    switch ($intent) {
+    switch ($intent) {
 
-        case 'listar':
+        case 'Episodios':
         {
-           break;
-        }
+            $episodios = json_decode(file_get_contents('assets/json/episodios.json'));
 
-        case 'criar':
-        {
+            $entrevistado = $json->queryResult->parameters->entrevistado;
+
+            if (empty($entrevistado)) {
+
+                $episodio = $episodios[array_rand($episodios, 1)];
+
+            } else {
+                $nomes = array_column($episodios, 'Entrevistado');
+                $episodio = $episodios[array_search($entrevistado, $nomes)];
+            }
+
+            $speech = "The Velopers #" . $episodio['Episodio'] .
+                ' - Entrevista com: ' . $episodio['Entrevistado'] .
+                ' - ' . $episodio['Descricao'] . ' - Assista pelo link: ' .
+                $episodio['Link'];
+
+
             break;
-
         }
 
-        case 'deletar':
-        {
 
-
-        }
         default:
             $speech = 'nenhuma intenção corresponde a sua frase.';
     }
@@ -43,10 +52,10 @@ if ($method == 'POST') {
       }
     }
   ]
-}';}
-*/
+}';
 
-    /*
+
+
     $response = '{
   "fulfillmentMessages": [
     {
@@ -58,7 +67,7 @@ if ($method == 'POST') {
     }
   ]
 }';
-    echo $response;*/
+    echo $response;
     echo $requestBody;
 } else {
     echo 'Método não aceito';
