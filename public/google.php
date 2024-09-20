6455acf402aaefc019fa88f32c9c74048b986f1f
Pokemaobr <contato@pokemaobr.dev>
2024-09-20 03:42:28 -0300

    atualizando

--- public/google.php
+++ public/google.php
@@ -8,8 +8,8 @@ if ($method == 'POST') {
 
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
-    $intent = $json->queryResult->intent->displayName;
-    
+    //$intent = $json->queryResult->intent->displayName;
+
 
 /*    switch ($intent) {
 
