ffc0ba61aa76559707ef6a454b907a7a6857650d
Pokemaobr <contato@pokemaobr.dev>
2024-09-20 03:18:05 -0300

    adicionando o bot do telegram

---
+++ public/google.php
@@ -0,0 +1,51 @@
+<?php
+
+set_time_limit(0);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+    $intent = $json->queryResult->intent->displayName;
+    $response = $json;
+
+/*    switch ($intent) {
+
+        case 'listar':
+        {
+           break;
+        }
+
+        case 'criar':
+        {
+            break;
+
+        }
+
+        case 'deletar':
+        {
+
+
+        }
+        default:
+            $speech = 'nenhuma intenção corresponde a sua frase.';
+    }
+
+    $response = '{
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+          "' . $speech . '"
+        ]
+      }
+    }
+  ]
+}';}
+*/
+    echo $response;
+} else {
+    echo 'Método não aceito';
+}
